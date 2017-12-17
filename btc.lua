8bb94ad090f44a51aec0321efd653102df4288eb
erdemsword <erik001.ronaldinho@gmail.com>
2017-12-17 19:21:38 +0300

    updated v1.0 to v1.1

--- btc.lua
+++ btc.lua
@@ -1,19 +1,15 @@
-db_name = "bitcoin" -- Veritabanı adı
-host = "localhost" -- Hosting adı "localhost"
-user = "root" -- Kullanıcı adı
-password = "" -- Şifre
+db_name = "bitcoin" 
+host = "localhost" 
+user = "root" 
+password = "" 
   
-database = dbConnect( "mysql", "dbname="..db_name..";host="..host, user, password ) -- Veritabanına bağlanma
-
---Veritabanına bağlanma kontrolü--
+database = dbConnect( "mysql", "dbname="..db_name..";host="..host, user, password ) 
 if database then 
     outputDebugString ('basarili') 
 else 
     outputDebugString ("basarisiz") 
 end
---
 
---Oyuncu sunucudan çıkınca otomatik veritabanına kaydetme--
 function saveBitcoins () 
     local serial = getPlayerSerial(source)
 	local bitcoin = getElementData(source, "bitcoin")
@@ -23,9 +19,7 @@ function saveBitcoins ()
       	dbExec ( database, "UPDATE userbitcoins SET userBitcoin = ? WHERE serial = ?", bitcoin, serial) 
     end
 end
-addEventHandler ( "onPlayerQuit", getRootElement(), saveBitcoins ) 
 
---Oyuncu sunucuya girdiğinde otomatik BTC tanıma--
 function loadBitcoins ()
     local serial = getPlayerSerial(source)
 	local bitcoin = getElementData(source, "bitcoin")
@@ -40,43 +34,21 @@ function loadBitcoins ()
         	setElementData ( source, "bitcoin", poll[1]["userBitcoin"] )
     end
 end
-addEventHandler ( "onPlayerJoin", getRootElement(), loadBitcoins ) 
---
+addEventHandler ( "onPlayerLogin", getRootElement(), loadBitcoins ) 
+addEventHandler ( "onPlayerQuit", getRootElement(), saveBitcoins ) 
 
---Servera BTC yükleme sistemi--
+ 
+
 function addBitcoinToServer(player, command, miktar)
-
+	if not miktar then outputChatBox("[HATA]#ffffff hata!",player,255,100,100,true) return end
 	local result = dbQuery ( database ,"SELECT * FROM serverbitcoins")
 	local poll, rows = dbPoll(result, -1)
-	local btc = poll[1]["totalBitcoin"]
-	if rows == 1 then
-		dbExec ( database, "UPDATE serverbitcoins SET totalBitcoin = btc+tostring(miktar)")
-		outputChatBox("[BITCOIN]#ffffff Sunucuya #ffaacc"..tostring(miktar).."BTC #ffffffüretildi!", root, 255, 100, 100, true)
+	if (rows>0) then
+		local btc = poll[1]['totalBitcoin']
+		dbExec ( database, "UPDATE serverbitcoins SET totalBitcoin = '"..btc.."'+'"..tostring(miktar).."'")
+		outputChatBox("[BITCOIN]#ffffff Sunucuya #ffaacc"..tostring(miktar).."BTC #ffffffyüklendi!", root, 255, 100, 100, true)
 	else
-		dbExec( database, "INSERT INTO serverbitcoins ( totalBitcoin) VALUES ( ?)", tostring(miktar) )
+		dbExec( database, "INSERT INTO serverbitcoins (totalBitcoin) VALUES ('"..tostring(miktar).."')" )
 	end
 end
 addCommandHandler("addbtc", addBitcoinToServer)
---
-
---Örnek geliştirilebilir BTC gönderme sistemi -- 
-function giveBTCToPlayer(player, command, playerName, miktar)
-	if not playerName then outputChatBox("[HATA] #ffffffOyuncu ismi girmelisin!", player, 255, 100, 100, true) return end
-	local playername = findPlayerByName(playerName)
-	setElementData(playername,"bitcoin" getElementData()
-end
-addCommandHandler("givebtc", giveBTCToPlayer)
---
-
---Oyuncu Bulma fonksiyonu--
-function findPlayerByName (name)
-	local player = getPlayerFromName(name)
-	if player then return player end
-	for i, player in ipairs(getElementsByType("player")) do
-		if string.find(string.gsub(getPlayerName(player):lower(),"#%x%x%x%x%x%x", ""), name:lower(), 1, true) then
-			return player
-		end
-	end
-return false
-end
---
